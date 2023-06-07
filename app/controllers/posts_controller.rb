f32becc9334cbb624d0b648fe067b1bfb8b72f5c
Richard Ten <rten04@icloud.com>
2023-06-07 13:31:12 -0400

    Add actions for Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post and return
+        end
+        render :new, status: :unprocessable_entity
+    end
+
+    private
+        def post_params
+
+        end
+    
 end
