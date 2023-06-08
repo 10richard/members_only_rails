8bf0cc460703ebdcd167256206dfa71caf662f2a
Richard Ten <rten04@icloud.com>
2023-06-08 08:33:49 -0400

    Add comment to post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :user_signed_in?
 
     def index
         @posts = Post.all
@@ -19,7 +20,8 @@ class PostsController < ApplicationController
 
     private
         def post_params
-
+            #post_params.user_id = current_user.id
+            params.require(:post_params)
         end
     
 end
