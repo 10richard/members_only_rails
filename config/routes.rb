277467c0d8c877351aa12c5f23654779628aede6
Richard Ten <rten04@icloud.com>
2023-06-08 13:23:19 -0400

    Fix mistype

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessoions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
   resources :posts, only: [:index, :new, :create]
   root to: 'posts#index'
