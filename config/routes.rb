1bd7ea41d31e485d2c61997648551108090d17f5
Richard Ten <rten04@icloud.com>
2023-06-08 12:26:25 -0400

    Add sessions controller to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessoions: 'users/sessions', registrations: 'users/registrations' }
 
   resources :posts, only: [:index, :new, :create]
   root to: 'posts#index'
